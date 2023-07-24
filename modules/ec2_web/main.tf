0a0d7b8ee569deea0a75f151472112ed31804cb3
Heitor Perozini <heitor.p@outlook.com>
2023-07-24 17:59:18 -0300

    Adding AWS templates

--- modules/ec2_web/main.tf
+++ modules/ec2_web/main.tf
@@ -1,8 +1,17 @@
 ################################################################################
 # EC2
 ###############################################################################
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+  }
+}
 resource "aws_instance" "web" {
-  ami           = "data.aws_ami.amazon_linux_2.id"
+  ami           = data.aws_ami.amazon_linux_2.id
   instance_type = "${var.ec2_instance_type}"
   subnet_id     = element(var.private_subnets, 0)
   key_name      = "${var.key_pair}"

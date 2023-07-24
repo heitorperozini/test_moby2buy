6d398f6d51b1563d20b89f948a99590d46868188
Heitor Perozini <heitor.p@outlook.com>
2023-07-24 19:46:15 -0300

    Adding AWS templates

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -99,6 +99,11 @@ resource "aws_route_table" "private" {
 
   vpc_id = aws_vpc.main.id
 
+    route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
   tags = merge(
     { "Name" = "PrivateRouteTable-${count.index}" },
     var.tags,

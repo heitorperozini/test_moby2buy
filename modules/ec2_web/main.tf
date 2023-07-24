22526c53e468a471416caac38129f04bb662744b
Heitor Perozini <heitor.p@outlook.com>
2023-07-24 19:56:01 -0300

    Adding AWS templates

--- modules/ec2_web/main.tf
+++ modules/ec2_web/main.tf
@@ -68,9 +68,16 @@ resource "aws_security_group" "load_balancer" {
   description = "Giving web access"
   vpc_id      = "${var.vpc_id}"
   ingress {
-    description = "Web"
+    description = "HTTP"
     from_port       = 80
     to_port         = 80
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+    ingress {
+    description = "HTTPS"
+    from_port       = 443
+    to_port         = 443
     protocol        = "tcp"
     cidr_blocks     = ["0.0.0.0/0"]
   }
